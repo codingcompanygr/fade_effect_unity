bbbf57681406b7decdb719e390fe8050c9d5929c
CodingCompany <122390758+codingcompanygr@users.noreply.github.com>
2023-03-24 03:13:32 +0200

    Update fade_anim.cs
    extra instructions

--- fade_anim.cs
+++ fade_anim.cs
@@ -1,3 +1,6 @@
+//You can change the if statment to whatever you want. It does not have to be a key input.
+//Also make sure that your animation are not set to loop. You can turn it off by checking off the box that says loop. I show this in the video.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
